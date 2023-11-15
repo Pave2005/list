1419ea8b3b242ab026ff57cb7bc728e6c755fa26
Pave2005 <142970374+Pave2005@users.noreply.github.com>
2023-11-15 14:40:16 +0300

    debug

--- list.h
+++ list.h
@@ -7,8 +7,6 @@ const int size = 20;
 enum LIST
 {
     NULL_ELEM = -8,
-    END = 0,
-    FREE = -1,
 };
 
 struct ListNode
@@ -23,12 +21,8 @@ struct List
     ListNode* list_elem;
     Elem_list head;
     Elem_list tail;
-    size_t count_of_calls;
-    size_t returned_place;
 };
 
-//size_t ReplaceInput (List* list, size_t place_before);
-//size_t List_Search (List* list, size_t search_location);
 void Insert (List* list, Elem_list num, size_t end_place);
 void ListDtor (List* list);
 void ListCtor (List* list);
